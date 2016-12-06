66b9c654c14aec03bfa659470b6c9b20c8bb26f3
ina <ina>
2016-12-07 02:39:29 +0900

    works

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
-	"k8s.io/client-go/pkg/api"
+	//"k8s.io/client-go/pkg/api/meta"
+	//"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/watch"
 
 	"k8s.io/client-go/rest"
@@ -32,20 +33,38 @@ func main() {
 	for {
 		select {
 		case event := <-eventCh:
-			ref, err := api.GetReference(event.Object)
+			/*
+			obj, err := meta.Accessor(event.Object);
 			if err == nil {
 				switch event.Type {
 				case watch.Added:
-					fmt.Printf("Added: %v\n", ref.Kind);
+					fmt.Printf("Added: %v\n", obj.GetName());
 				case watch.Modified:
-					fmt.Printf("Modified: %v\n", ref.Kind);
+					fmt.Printf("Modified: %v\n", obj.GetName());
 				case watch.Deleted:
-					fmt.Printf("Deleted: %v\n", ref.Kind);
+					fmt.Printf("Deleted: %v\n", obj.GetName());
 				case watch.Error:
-					fmt.Printf("Error: %v\n", ref.Kind);
+					fmt.Printf("Error: %v\n", obj.GetName());
 				}
 			}else{
 				fmt.Printf("%v %v\n", event.Type, err);
+			}*/
+			ev, ok := event.Object.(*v1.Event)
+			if ok {
+				//fmt.Printf("Name: %v, Message: %v", ev.Name, ev.Message);
+				switch event.Type {
+				case watch.Added:
+					fmt.Printf("Added: %v %v\n", ev.Name, ev.Message);
+				case watch.Modified:
+					fmt.Printf("Modified: %v %v\n", ev.Name, ev.Message);
+				case watch.Deleted:
+					fmt.Printf("Deleted: %v %v\n", ev.Name, ev.Message);
+				case watch.Error:
+					fmt.Printf("Error: %v %v\n", ev.Name, ev.Message);
+				}
+
+			} else{
+				fmt.Printf("error\n");
 			}
 
 		}
